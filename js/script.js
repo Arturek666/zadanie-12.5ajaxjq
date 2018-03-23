adf5ffb77609b59000c124c8cc944c6693dc1ce4
Artur Schulz <artur.schulz123@gmail.com>
2018-03-23 16:41:01 +0100

    add files

--- js/script.js
+++ js/script.js
@@ -18,19 +18,20 @@ function createTweet(input) {
 
 
     }
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-    if (tweetText.length > 140) {
-        getQuote();
-    } else {
-        var tweet = tweetLink + encodeURIComponent(tweetText);
-        $('.quote').text(quoteText);
-        $('.author').text("Author: " + quoteAuthor);
-        $('.tweet').attr('href', tweet);
-    }
-    $(document).ready(function() {
-        getQuote();
-        $('.trigger').click(function() {
-            getQuote();
-        })
-    });
 }
+var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+if (tweetText.length > 140) {
+    getQuote();
+} else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    $('.quote').text(quoteText);
+    $('.author').text("Author: " + quoteAuthor);
+    $('.tweet').attr('href', tweet);
+}
+
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
+});
