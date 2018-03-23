27dbacb72824a333625cf0b5317a5ffde2ad587a
Artur Schulz <artur.schulz123@gmail.com>
2018-03-23 17:13:00 +0100

    add files

--- js/script.js
+++ js/script.js
@@ -6,34 +6,34 @@ function getQuote() {
     $.getJSON(prefix + quoteUrl, createTweet);
     $.ajaxSetup({ cache: false });
 
+}
 
-    function createTweet(input) {
-        var data = input[0];
+function createTweet(input) {
+    var data = input[0];
 
-        var quoteText = $(data.content).text().trim();
-        var quoteAuthor = data.title;
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
 
-        if (!quoteAuthor.length) {
-            quoteAuthor = "Unknown author";
-
-
-        }
-
-        var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-        if (tweetText.length > 140) {
-            getQuote();
-        } else {
-            var tweet = tweetLink + encodeURIComponent(tweetText);
-            $('.quote').text(quoteText);
-            $('.author').text("Author: " + quoteAuthor);
-            $('.tweet').attr('href', tweet);
-        }
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
 
 
-        $(document).ready(function() {
-            getQuote();
-            $('.trigger').click(function() {
-                getQuote();
-            })
-        });
     }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
+    }
+
+
+    $(document).ready(function() {
+        getQuote();
+        $('.trigger').click(function() {
+            getQuote();
+        })
+    });
