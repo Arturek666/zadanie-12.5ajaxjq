a2855f967ca84a687f34885f21e483330e3095e9
Artur Schulz <artur.schulz123@gmail.com>
2018-03-23 16:42:55 +0100

    add files

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ var prefix = "https://cors-anywhere.herokuapp.com/";
 function getQuote() {
     $.getJSON(prefix + quoteUrl, createTweet);
     $.ajaxSetup({ cache: false });
-}
-
-function createTweet(input) {
-    var data = input[0];
-
-    var quoteText = $(data.content).text().trim();
-    var quoteAuthor = data.title;
-
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
 
 
+    function createTweet(input) {
+        var data = input[0];
+
+        var quoteText = $(data.content).text().trim();
+        var quoteAuthor = data.title;
+
+        if (!quoteAuthor.length) {
+            quoteAuthor = "Unknown author";
+
+        }
     }
 }
 var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
